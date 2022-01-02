660bfcc0187a9b644ccef9963d4e8a6f7912a8f3
yurikgs <yurikgs@outlook.com>
2022-01-02 01:25:11 -0300

    codeflix updates

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,79 @@
-
 //olw carousel setup
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin: 10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:7
+$(() => {
+
+    //nav icon setup consts
+    const prevIcon = '<img src="/img/icons/prev-icon.svg" alt:"prev" >';
+    const nextIcon = '<img src="/img/icons/next-icon.svg" alt:"next" >';
+
+    $('#carousel-small-1').owlCarousel({
+        nav: true,
+        loop: true,
+        margin: 200,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 3500, //dessincronizar carrosséis
+        smartSpeed: 800,
+        navText: [
+            prevIcon,
+            nextIcon
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
         }
-    }
-})
+    })
+
+    $('#carousel-small-2').owlCarousel({
+        nav: true,
+        loop: true,
+        margin: 200,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 4200, //dessincronizar carrosséis
+        smartSpeed: 800,
+        navText: [
+            prevIcon,
+            nextIcon
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+
+    $('#carousel-big-1').owlCarousel({
+        nav: false,
+        loop: true,
+        margin: 200,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000, //dessincronizar carrosséis
+        smartSpeed: 1800,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
